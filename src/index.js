30a18002f137647a2f61aeed7d7944185670445d
Amber-Pittman <amber@amberpittman.com>
2019-09-06 19:58:23 -0400

    Met MVP

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-<Router> 
-    <App /> 
-</Router>
+    <Router> 
+        <App /> 
+    </Router>
 , document.getElementById('root'));
